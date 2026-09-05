89c9e1860bcf181d261a9d0405c814f46dad3a98
Andrews Matheus <84197804+AndrewsMatheus@users.noreply.github.com>
2026-09-05 13:41:18 -0400

    Fix Netlify deployment config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,20 @@ const nextConfig: NextConfig = {
   agentRules: false,
   outputFileTracingRoot: process.cwd(),
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/players',
+        destination: '/ranking',
+        permanent: true,
+      },
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
